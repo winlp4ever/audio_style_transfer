cd6fb59e880ad64d5e31881b513eaa3fcdc9ff5e
Le Ha Quang <winlp4ever@gmail.com>
2018-06-22 22:36:10 +0200

    temp change

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source ~/.bashrc
-gpucentos
-. /home/wp01/interns/leh_2018/anaconda3/etc/profile.d/conda.sh
-source activate /home/wp01/interns/leh_2018/anaconda3/envs/deep_learning
-python anet.py 'bass' 0 0 --epochs 300 --layers 17 19
