06adb962187069b1f8501656dbe2bfe5f35c6eb0
Le Ha Quang <winlp4ever@gmail.com>
2018-06-28 00:25:10 +0200

    add my_nmf.py

---
+++ my_nmf.py
@@ -0,0 +1,91 @@
+import tensorflow as tf
+import numpy as np
+import time
+from numpy.linalg import norm
+
+from sklearn.decomposition import NMF
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+class MyNMF(object):
+    def __init__(self, n_components, size_in, learning_rate=1e-4):
+        assert len(size_in) == 2
+        f, t = size_in
+
+        tf.reset_default_graph()
+        mat = tf.placeholder(tf.float32, shape=size_in, name='mat')
+        W = tf.Variable(tf.random_uniform([f, n_components], minval=0., maxval=1.), dtype=tf.float32, name='W')
+        H = tf.Variable(tf.random_uniform([n_components, t], minval=0., maxval=1.), dtype=tf.float32, name='H')
+
+        clip_W = W.assign(tf.maximum(tf.zeros_like(W), W))
+        clip_H = H.assign(tf.maximum(tf.zeros_like(H), H))
+        clip = tf.group(clip_W, clip_H)
+
+        with tf.name_scope('loss'):
+            xent = tf.norm(mat - tf.matmul(W, H))
+            tf.summary.scalar('nmf_loss', xent)
+
+
+        with tf.name_scope('optim'):
+            optimizer = tf.contrib.opt.ScipyOptimizerInterface(
+                xent,
+                method='L-BFGS-B',
+                options={'maxiter': 100})
+
+        self.learning_rate = learning_rate
+        self.size_in = size_in
+        self.graph = {'mat_in' : mat,
+                      'w' : W,
+                      'h' : H,
+                      'loss': xent,
+                      'optim' : optimizer,
+                      'clip': clip}
+
+    def fit_transform(self, X, epochs):
+        with tf.Session() as sess:
+
+            i = 0
+            def loss_tracking(loss_):
+                nonlocal i
+                if not i % 5:
+                    tf.logging.info(' Step: {} -- Loss: {}'.format(i, loss_))
+                i += 1
+
+            sess.run(tf.global_variables_initializer())
+
+            while i < epochs:
+                self.graph['optim'].minimize(sess,
+                                             loss_callback=loss_tracking,
+                                             fetches=[self.graph['loss']],
+                                             feed_dict={self.graph['mat_in']: X})
+                sess.run(self.graph['clip'])
+
+            tf.logging.info(' FINAL LOSS: {}'.format(sess.run(self.graph['loss'],
+                                                              feed_dict={self.graph['mat_in']: X})))
+            W, H = sess.run([self.graph['w'], self.graph['h']])
+            print('my nmf error : {}'.format(norm(X - np.matmul(W, H))))
+        return W / norm(W), H * norm(W)
+
+
+def main():
+    np.random.seed()
+    n_components = 128
+    size_in = [128, 320000]
+    X = np.random.sample(size_in)
+    nmf = MyNMF(n_components, size_in)
+    since = time.time()
+    W, H = nmf.fit_transform(X, epochs=10)
+
+    assert (W >= 0).all() and (H >= 0).all()
+    print('time-lapse {}'.format(time.time() - since))
+
+
+    nmf_ = NMF(n_components=n_components, init='random', random_state=0, max_iter=400, solver='mu')
+    since_ = time.time()
+    W_ = nmf_.fit_transform(X)
+    H_ = nmf_.components_
+    print('time-lapse {}'.format(time.time() - since_))
+    print('error: {}'.format(norm(X - np.matmul(W_, H_))))
+
+if __name__ ==  '__main__':
+    main()
--- test.py
+++ test.py
@@ -3,15 +3,10 @@ from numpy.linalg import norm
 import time
 from sklearn.decomposition import NMF
 
-np.random.seed()
-tt = 0
-nmf = NMF(n_components=128, init='random', random_state=0, solver='mu')
-for i in range(2):
-    print(i)
-    a = np.random.sample([128, 320000])
-    since = time.time()
-    W = nmf.fit_transform(a)
-    H = nmf.components_
-    tt +=time.time() - since
+import tensorflow as tf
 
-print(tt/2)
+u = tf.constant(3.)
+
+with tf.Session() as sess:
+    with tf.Session() as sess_:
+        print(sess.run(u))
