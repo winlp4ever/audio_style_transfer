051d9b25b11ad418dadf017a6565ceeb97ca7a6e
Le Ha Quang <winlp4ever@gmail.com>
2018-07-28 13:27:39 +0200

    change tests - fix err

--- runGatys.sh
+++ runGatys.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for s in cat-mil crickets drums; do
+for s in cat-milk crickets drums; do
     for l in 0.5 1 5 10; do
         for g in 1 10 20 50 100; do
             python gatys.py female $s --lambd $l --gamma $g --epochs 100 --batch_size $(( 16384 * 3 ))
