6c4df6aa2d3151de0a91b9f0db62befff9b794c9
Le Ha Quang <winlp4ever@gmail.com>
2018-08-04 01:16:18 +0200

    modify tests

--- runGatys.sh
+++ runGatys.sh
@@ -2,7 +2,7 @@
 for c in pachelbel female canon; do
     for s in gui_solo_mono crickets drums cat-milk exo_flute bongo-loop; do
         for g in 10 20 50; do
-            python gatys.py $c $s --epochs 100 --cont_lyrs 25 --stack 0 --batch_size $((16384*2)) --lambd 100 --gamma $g
+            python gatys.py $c $s --epochs 100 --cont_lyrs 25 --batch_size $((16384*2)) --lambd 100 --gamma $g
         done
     done
 done
--- runf.sh
+++ runf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-for s in drums crickets voc_mono orchestra; do
-    for c in pachelbel female; do
-        python khac.py $c $s --epochs 100 --cont_lyrs 24 --stack 0 --lambd 500 --gamma 100 --batch_size 8192 --duration 2
+for s in pachelbel female; do
+    for c in crickets drums cat-milk bongo-loop exo_flute; do
+        python acrossLayers.py $c $s --epochs 100 --cont_lyrs 24 --stack 0 --lambd 100 --gamma 20 --batch_size $((16384*2)) --channels 64
     done
 done
