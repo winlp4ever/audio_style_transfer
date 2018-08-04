33fc9e88b8871ac5ac9499acd0a1ad8689a0af2a
Le Ha Quang <winlp4ever@gmail.com>
2018-08-04 22:09:54 +0200

    modify tests

--- runGatys.sh
+++ runGatys.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-for c in pachelbel female canon; do
-    for s in gui_solo_mono crickets drums cat-milk exo_flute bongo-loop; do
-        for g in 10 20 50; do
-            python gatys.py $c $s --epochs 100 --cont_lyrs 25 --batch_size $((16384*2)) --lambd 100 --gamma $g
-        done
+for s in gui_solo_mono crickets drums cat-milk exo_flute bongo-loop; do
+    for g in 10 20 50; do
+            python gatys.py $1 $s --epochs 100 --cont_lyrs 25 --batch_size $((16384*2)) --lambd 100 --gamma $g
     done
 done
