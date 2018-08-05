db671f62f5be51ea7fb4f9b957b3ecbb62e40882
Le Ha Quang <winlp4ever@gmail.com>
2018-08-05 13:01:56 +0200

    output spectrogram

--- te.py
+++ te.py
@@ -8,7 +8,7 @@ for dir in glob.glob('./data/out/84/**'):
     base = os.path.basename(dir)
     if 'gatys' in base:
         print(dir)
-        for f in glob.glob(dir + '**'):
+        for f in glob.glob(dir + '/**'):
             if f.endswith('ori.wav'):
                 fn = os.path.basename(f)
                 spectrogram.plotstft(f, plotpath=os.path.join(figdir, os.path.join(base, fn + '.wav')))
